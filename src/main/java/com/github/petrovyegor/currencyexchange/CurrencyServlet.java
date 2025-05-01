8e0bc85ec6cfec0d95443efd931bbc261c769a51
PetrovYegor <YegorPetrov@outlook.com>
2025-05-02 00:33:14 +0300

    реализовал post метод по добавлению новой валюты в БД, в таблицу Currencies

--- src/main/java/com/github/petrovyegor/currencyexchange/CurrencyServlet.java
+++ src/main/java/com/github/petrovyegor/currencyexchange/CurrencyServlet.java
@@ -31,7 +31,7 @@ public class CurrencyServlet extends HttpServlet {
         }
 
         String[] parts = path.split("/");
-        if (parts.length < 2 ) {
+        if (parts.length < 2) {
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid currency code");
             return;
         }
@@ -47,8 +47,93 @@ public class CurrencyServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
+        String code = request.getParameter("code");
+        String name = request.getParameter("name");
+        String sign = request.getParameter("sign");
+
+        if (code == null || name == null || sign == null) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing required fields");
+            return;
+        }
+
+        if (code.length() != 3) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Currency code must be 3 letters");
+            return;
+        }
+
+        if (name.length() > 50) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Currency full name must be less 50 letters");
+            return;
+        }
+
+        if (sign.length() > 10) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Currency sign must be less 10 letters");
+            return;
+        }
+
+        try {
+            if (currencyExists(code)) {
+                response.sendError(HttpServletResponse.SC_CONFLICT, "Currency already exists");
+                return;
+            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        Currency currency = null;
+        try {
+            currency = saveCurrency(code, name, sign);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        // 5. Возвращаем созданную валюту в JSON
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_CREATED); // 201 Created
+        new ObjectMapper().writeValue(response.getWriter(), currency);
+    }
+
+    private Currency saveCurrency(String code, String name, String sign) throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        Connection co = DriverManager.getConnection("jdbc:sqlite:C:\\roadmap\\CurrencyExchange\\CurrencyExchange.sqlite");
+        //String query = "INSERT INTO Currencies (Code, FullName, Sign) VALUES (?, ?, ?);";
+        String query = "INSERT INTO Currencies (Code, FullName, Sign) VALUES ('AGA', 'TEST', '???')";
+        try (co; PreparedStatement statement = co.prepareStatement(query)) {
+//            statement.setString(1, code.toUpperCase());
+//            statement.setString(2, name);
+//            statement.setString(3, sign);
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+
+            if (resultSet.next()) {
+                int id = resultSet.getInt(1);
+                return new Currency(id, code, name, sign);
+            } else {
+                throw new SQLException("Failed to get generated ID");
+            }
+        }
+    }
+
+    private boolean currencyExists(String code) throws IOException, ClassNotFoundException, SQLException {
+        //добавить проверку регуляркой на англ буквы 3 знака
+        Class.forName("org.sqlite.JDBC");
+        try (        Connection co = DriverManager.getConnection("jdbc:sqlite:C:\\roadmap\\CurrencyExchange\\CurrencyExchange.sqlite");) {
+            //String query = "SELECT Id FROM Currencies WHERE Code = ?";
+            String query = "SELECT id FROM currencies WHERE code = ?";
+            PreparedStatement statement = co.prepareStatement(query);
+            statement.setString(1, code.toUpperCase());
+            //return statement.executeQuery().next();
+            ResultSet rs = statement.executeQuery();
+            return rs.next();
+        }
     }
 
     private void getAllCurrencies(HttpServletResponse response) throws ClassNotFoundException, SQLException, IOException {
@@ -68,9 +153,10 @@ public class CurrencyServlet extends HttpServlet {
                 currencies.add(new Currency(id, code, fullName, sign));
             }
             response.setContentType("application/json");
-            response.setCharacterEncoding("UTF-8");;
+            response.setCharacterEncoding("UTF-8");
+            ;
             objectMapper.writeValue(response.getWriter(), currencies);
-            rs.close();//закрываем резалт сэт
+            rs.close();
 
         } catch (SQLException e) {
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
