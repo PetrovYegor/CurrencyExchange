3fb69e4592e6614b063349f55a0c05fb1276bc2c
PetrovYegor <YegorPetrov@outlook.com>
2025-05-16 23:45:45 +0300

    новый сервлет для конвертации валюты

--- src/main/java/com/github/petrovyegor/currencyexchange/controller/ConversionController.java
+++ src/main/java/com/github/petrovyegor/currencyexchange/controller/ConversionController.java
@@ -1,4 +1,55 @@
 package com.github.petrovyegor.currencyexchange.controller;
 
-public class ConversionController {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.petrovyegor.currencyexchange.dto.ExchangeRateResponseDTO;
+import com.github.petrovyegor.currencyexchange.model.Currency;
+import com.github.petrovyegor.currencyexchange.service.ExchangeRateService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class ConversionController extends HttpServlet {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ExchangeRateService exchangeRateService = new ExchangeRateService();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String path = request.getPathInfo();
+        if (path != null) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write("Invalid parameters");
+        }
+        String baseCurrencyCode = request.getParameter("from");
+        String targetCurrencyCode = request.getParameter("to");
+        String amountString = request.getParameter("amount");
+
+        if (baseCurrencyCode == null || targetCurrencyCode == null || amountString == null) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing required fields!");
+            return;
+        }
+        try {
+            Currency baseCurrency = exchangeRateService.getCurrencyByCode(baseCurrencyCode);
+            Currency targetCurrency = exchangeRateService.getCurrencyByCode(targetCurrencyCode);
+
+            ExchangeRateResponseDTO exchangeRate = exchangeRateService.getByCurrencies(baseCurrency, targetCurrency);
+//            if (exchangeRate == null) {
+//                response.sendError(404, "Exchange rate doesn't exists!");
+//                return;
+//            }
+
+            exchangeRate = exchangeRateService.updateRate(rate, exchangeRate.getId(), baseCurrency, targetCurrency);
+            response.setStatus(200);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            objectMapper.writeValue(response.getWriter(), exchangeRate);
+        } catch (SQLException e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write("{\"error\":\"Database error: " + e.getMessage() + "\"}");
+        }
+
+
+    }
 }
